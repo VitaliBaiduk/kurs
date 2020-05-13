10c8733efedcfd0f1b602023522224ecbfcdf866
Vitali <vitalikbaiduj@gmail.com>
2020-05-13 23:35:29 +0300

    update

--- file.js
+++ file.js
@@ -1,1 +1,16 @@
 "use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieOB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Один из просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieOB.movies[a] = b;
+
