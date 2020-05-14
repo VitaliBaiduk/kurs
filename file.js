38b0056832b541c74700f5629b751a1d4e649782
Vitali <vitalikbaiduj@gmail.com>
2020-05-14 20:37:38 +0300

    new update

--- file.js
+++ file.js
@@ -7,10 +7,28 @@ const personalMoviesDB = {
   genres: [],
   private: false,
 };
-const a = prompt("Один из просмотренных фильмов?", ""),
-  b = prompt("На сколько вы его оцените?", ""),
-  c = prompt("Один из просмотренных фильмов?", ""),
-  d = prompt("На сколько вы его оцените?", "");
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из просмотренных фильмов?", ""),
+    b = prompt("На сколько вы его оцените?", "");
+
+  if (a != null && b != null && a != false && b != false && a.length < 50) {
+    personalMoviesDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalMoviesDB.count <= 10) {
+  console.log("Просмотренно довольно мало фильмов");
+} else if (personalMoviesDB.count > 10 && personalMoviesDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMoviesDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
 console.log(personalMoviesDB);
